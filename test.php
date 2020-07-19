bf94f7e38ea11ba0c2c719677080aef181d9285a
Nicholas <22617624+Ch1x3@users.noreply.github.com>
2020-07-19 22:37:21 +0400

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
   <body>
     <form action="/wow" method="GET">
       <input type="text" name="test">
-      <button type="submit">
+      <button type="submit"></button>
         </form>
   </body>
       
