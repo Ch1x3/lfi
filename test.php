cacd30fca672cc0a7ae2a46b03aaae1697adf935
Nicholas <22617624+Ch1x3@users.noreply.github.com>
2020-07-19 23:05:07 +0400

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,31 @@
-<html>
-  <body>
-    <form action="/wow" method="GET">
-      <input type="text" name="test">
-      <button name="hello" type="submit">
-        </form>
-  </body>
-      
+<?php
+
+echo "<br/>";
+
+echo "Hello, Welcome to infosec news";
+
+echo "<br/>";
+
+echo "<br/>";
+
+echo "<html>
+
+<body>
+
+<a href=”index.php?page=news.php”><button>Show News</button></a>
+
+</body>
+
 </html>
+
+";
+
+echo "<br/>";
+
+echo "<br/>";
+
+include("/flag.txt");
+
+echo "<br/>";
+
+?>
